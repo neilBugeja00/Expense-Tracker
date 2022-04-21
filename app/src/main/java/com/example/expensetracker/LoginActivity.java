c8fd0e8a501659bee4b369b62ee489fe66feff26
neilBugeja00 <neilbugeja2000@gmail.com>
2022-04-21 17:49:28 +0200

    Created login and sign in page

---
+++ app/src/main/java/com/example/expensetracker/LoginActivity.java
@@ -0,0 +1,31 @@
+package com.example.expensetracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+
+        //Make button "createAccount" move to "SignUpActivity.java"
+        Button callSignUp = (Button)findViewById(R.id.createAccount);
+
+        callSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, SignUpActivity.class));
+                finish();
+            }
+        });
+    }
+
+
+}
---
+++ app/src/main/java/com/example/expensetracker/SignUpActivity.java
@@ -0,0 +1,32 @@
+package com.example.expensetracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+
+
+        //Make button "callLogin" move to "LoginActivity.java"
+        Button callLogin = (Button)findViewById(R.id.loginButton);
+
+        callLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(SignUpActivity.this, LoginActivity.class));
+                finish();
+            }
+        });
+
+    }
+}
