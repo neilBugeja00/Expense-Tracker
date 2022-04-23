2381e26d6a0d5617f4499cead76e5223ba43c69e
neilBugeja00 <neilbugeja2000@gmail.com>
2022-04-23 23:55:42 +0200

    Working creating user version 2 With validation

--- app/src/main/java/com/example/expensetracker/LoginActivity.java
+++ app/src/main/java/com/example/expensetracker/LoginActivity.java
@@ -1,22 +1,49 @@
 package com.example.expensetracker;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText editEmail, editPassword;
+    private Button signIn, callSignUp;
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        //Button "signInButton"
+        signIn = (Button)findViewById(R.id.signInButton);
+        editEmail = (EditText) findViewById(R.id.email);
+        editPassword = (EditText) findViewById(R.id.password);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        signIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                userLogin();
+            }
+        });
 
         //Make button "createAccount" move to "SignUpActivity.java"
-        Button callSignUp = (Button)findViewById(R.id.createAccount);
+        callSignUp = (Button)findViewById(R.id.createAccount);
 
         callSignUp.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -27,5 +54,38 @@ public class LoginActivity extends AppCompatActivity {
         });
     }
 
+    private void userLogin(){
+        String email = editEmail.getText().toString().trim();
+        String password = editPassword.getText().toString().trim();
+
+        if(email.isEmpty()){
+            editEmail.setError("Email required");
+            editEmail.requestFocus();
+            return;
+        }
+        if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            editEmail.setError("Please provide valid email");
+            editEmail.requestFocus();
+            return;
+        }
+
+        if(password.isEmpty()){
+            editPassword.setError("Full name required");
+            editPassword.requestFocus();
+            return;
+        }
+
+        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    //redirect to user profile
+                    startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                }else{
+                    Toast.makeText(LoginActivity.this, "Failed to Login! Please check credentials", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
 
 }
--- app/src/main/java/com/example/expensetracker/SignUpActivity.java
+++ app/src/main/java/com/example/expensetracker/SignUpActivity.java
@@ -1,39 +1,43 @@
 package com.example.expensetracker;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class SignUpActivity extends AppCompatActivity {
 
     //Variables
-    TextInputLayout regName, regUsername, regEmail, regPassword;
+    private EditText editName, editEmail, editPassword;
     Button regBtn, callLogin;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
 
-        //linking
-        EditText editName = findViewById(R.id.regName);
-        EditText editUsername = findViewById(R.id.regUsername);
-        EditText editEmail = findViewById(R.id.regEmail);
-        EditText editPassword = findViewById(R.id.regPassword);
+        //initialise authentication
+        mAuth = FirebaseAuth.getInstance();
 
-        //regName = findViewById(R.id.regName);
-        //regUsername = findViewById(R.id.regUsername);
-        //regEmail = findViewById(R.id.regEmail);
-        //regPassword = findViewById(R.id.regPassword);
+        //linking
+        editName = findViewById(R.id.regName);
+        editEmail = findViewById(R.id.regEmail);
+        editPassword = findViewById(R.id.regPassword);
 
         regBtn = (Button)findViewById(R.id.regButton);
         callLogin = (Button)findViewById(R.id.loginButton);
@@ -45,12 +49,66 @@ public class SignUpActivity extends AppCompatActivity {
         regBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                User user = new User(editName.getText().toString(),editUsername.getText().toString(),editEmail.getText().toString(),editPassword.getText().toString());
-                dao.add(user).addOnSuccessListener(suc ->{
-                   Toast.makeText(getApplicationContext(),"Record is inserted",Toast.LENGTH_SHORT).show();
-                }).addOnFailureListener(err ->{
-                    Toast.makeText(getApplicationContext(),""+err.getMessage(),Toast.LENGTH_SHORT).show();
-                });
+                String name = editName.getText().toString().trim();
+                String email = editEmail.getText().toString().trim();
+                String password = editPassword.getText().toString().trim();
+
+                //checking if variables are empty
+                if(name.isEmpty()){
+                    editName.setError("Full name required");
+                    editName.requestFocus();
+                    return;
+                }
+
+                if(email.isEmpty()){
+                    editEmail.setError("Email required");
+                    editEmail.requestFocus();
+                    return;
+                }
+                if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+                    editEmail.setError("Please provide valid email");
+                    editEmail.requestFocus();
+                    return;
+                }
+
+                if(password.isEmpty()){
+                    editPassword.setError("Password required");
+                    editPassword.requestFocus();
+                    return;
+                }
+                if(password.length()<6){
+                    editPassword.setError("Min password length must be 6 characters!");
+                    editPassword.requestFocus();
+                    return;
+                }
+
+                mAuth.createUserWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if(task.isSuccessful()){
+                                    User user = new User(name,email);
+
+                                    FirebaseDatabase.getInstance("https://expensetracker-8ed93-default-rtdb.europe-west1.firebasedatabase.app").getReference(User.class.getSimpleName())
+                                            .child(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                                            .setValue(user).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                        @Override
+                                        public void onComplete(@NonNull Task<Void> task) {
+                                            if(task.isSuccessful()){
+                                                Toast.makeText(SignUpActivity.this, "User registered successfully!", Toast.LENGTH_LONG).show();
+
+                                                //redirect to login layout!
+                                            }else{
+                                                Toast.makeText(SignUpActivity.this, "Failed to register! Try again", Toast.LENGTH_LONG).show();
+                                            }
+                                        }
+                                    });
+                                }else{
+                                    Toast.makeText(SignUpActivity.this, "Failed to register! Try again", Toast.LENGTH_LONG).show();
+                                }
+                            }
+                        });
+
             }
         });
 
