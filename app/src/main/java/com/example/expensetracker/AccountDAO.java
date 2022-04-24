8ccce412b275a80a6c95b8eb41f4e6d8c8454339
neilBugeja00 <neilbugeja2000@gmail.com>
2022-04-24 15:35:15 +0200

    Add Account Linked with user

--- app/src/main/java/com/example/expensetracker/AccountDAO.java
+++ app/src/main/java/com/example/expensetracker/AccountDAO.java
@@ -1,16 +1,21 @@
 package com.example.expensetracker;
 
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class AccountDAO {
-
     private DatabaseReference databaseReference;
 
     public AccountDAO(){
         FirebaseDatabase db = FirebaseDatabase.getInstance("https://expensetracker-8ed93-default-rtdb.europe-west1.firebasedatabase.app"); //gets root
-        databaseReference = db.getReference(Account.class.getSimpleName());
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        String userId = user.getUid();
+        databaseReference = db.getReference(Account.class.getSimpleName()).child(userId);
+
+
     }
 
     public Task<Void> add(Account account){
--- app/src/main/java/com/example/expensetracker/AccountFragment.java
+++ app/src/main/java/com/example/expensetracker/AccountFragment.java
@@ -1,8 +1,6 @@
 package com.example.expensetracker;
 
 import android.app.AlertDialog;
-import android.content.DialogInterface;
-import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
